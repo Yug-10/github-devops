f3c59be640634598b3e872cf56f378ce7fcd170b
yugpatel <patelyug2809@gmail.com>
2026-03-12 22:29:11 +0530

    commited

---
+++ ok.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class a{
+    int x;
+    Scanner sc =new Scanner(System.in);
+    
+    public void input() {
+        System.out.println("enter the value of x");
+        x=sc.nextInt();
+        if (x>0) {
+            System.out.println("number is Positive");
+        }
+        
+    }
+
+}
