894cd8ee45a82eeda1c901a3a8db40d2f49b7015
yugpatel <patelyug2809@gmail.com>
2026-03-12 22:41:34 +0530

    commited

--- ok.java
+++ ok.java
@@ -1,19 +1,34 @@
 import java.util.*;
-class a{
-    int x;
-    Scanner sc =new Scanner(System.in);
-    
-    public void input() {
-        System.out.println("enter the value of x");
-        x=sc.nextInt();
-        if (x>0) {
-            System.out.println("number is Positive");
-            
-        }
-        else{
-            System.out.println("number is Negative");
-        }
-        
-    }
 
+class a {
+    public static void main(String[] args) {
+        int day;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the day number");
+        day=sc.nextInt();
+        switch(day){
+            case 1:
+                System.out.println("monday");
+                break;
+            case 2:     
+                System.out.println("tuesday");
+                break;
+            case 3:
+                System.out.println("wednesday");
+                break;
+            case 4:
+                System.out.println("thursday");
+                break; 
+            case 5:
+                System.out.println("friday");
+                break;
+            case 6: 
+                System.out.println("saturday");      
+                break;
+            case 7:
+                System.out.println("sunday");
+
+        }
+       
+}
 }
